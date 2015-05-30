59273dbef5f84e6d54c0635aab56a1b3dc74315e
Brandon Collins <brandoncollins@gmail.com>
2015-05-30 15:10:00 -0400

    encrypt function

--- nxtvault/src/main/assets/jay/js/androidextensions.js
+++ nxtvault/src/main/assets/jay/js/androidextensions.js
@@ -12,8 +12,8 @@ var AndroidExtensions = {
 	storePin: function(pin){
 		var pinData = {};
 
-		pinData["cypher"] = encryptSecretPhrase(accountData["secretPhrase"], pin).toString();
-		pinData["checksum"] = converters.byteArrayToHexString(simpleHash(converters.stringToByteArray(accountData["secretPhrase"])));
+		pinData["cypher"] = encryptSecretPhrase("pin", pin).toString();
+		pinData["checksum"] = converters.byteArrayToHexString(simpleHash(converters.stringToByteArray(pin)));
 
 		localStorage["pin"] = JSON.stringify(pinData);
 	},
@@ -23,8 +23,8 @@ var AndroidExtensions = {
 
 		if (localStorage["pin"]){
 			var pinData = JSON.parse(localStorage["pin"]);
-			var cypher = encryptSecretPhrase(accountData["secretPhrase"], pin).toString();
-			var checksum = converters.byteArrayToHexString(simpleHash(converters.stringToByteArray(accountData["secretPhrase"])));
+			var cypher = encryptSecretPhrase("pin", pin).toString();
+			var checksum = converters.byteArrayToHexString(simpleHash(converters.stringToByteArray(pin)));
 
 			if (pinData["cypher"] === cypher && pinData["checksum"] == checksum){
 				result = true;
