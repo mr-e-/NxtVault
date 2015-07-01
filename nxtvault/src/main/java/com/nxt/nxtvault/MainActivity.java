40e69dad3b83451470cf04b3b6c2096b3e292a6c
Brandon Collins <brandoncollins@gmail.com>
2015-07-01 13:23:04 -0400

    resolve issue with diabling custom broadcast server

--- nxtvault/src/main/java/com/nxt/nxtvault/MainActivity.java
+++ nxtvault/src/main/java/com/nxt/nxtvault/MainActivity.java
@@ -71,14 +71,14 @@ public class MainActivity extends BaseActivity {
     }
 
     public void setServerInfo() {
-        if (mPreferences.getCustomServer() != null && mPreferences.getCustomServer() != ""){
+        if (mPreferences.getCustomServer() == null || mPreferences.getCustomServer().isEmpty()){
+            mJay.setRequestMethod(RequestMethods.Fastest);
+            mJay.setIsTestnet(false);
+        }
+        else{
             mJay.setNode(mPreferences.getCustomServer());
             mJay.setRequestMethod(RequestMethods.Single);
             mJay.setIsTestnet(mPreferences.getIsTestNet());
-        }
-        else{
-            mJay.setRequestMethod(RequestMethods.Fastest);
-            mJay.setIsTestnet(false);
         }
     }
 
